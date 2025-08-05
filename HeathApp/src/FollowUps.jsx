6ffe4ca6a047e4a5864de70bdc9fc43e989c6209
Lay balar <lay.balar05@gmail.com>
2025-08-05 18:05:49 +0530

    fix: move type: module to root

--- HeathApp/src/FollowUps.jsx
+++ HeathApp/src/FollowUps.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_BASE_URL;
+const API_URL = "https://heathapp-3a79.onrender.com";
 
 function FollowUps() {
   const { token } = useSelector((state) => state.auth);
