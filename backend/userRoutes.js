56c0df7347615bead744802e849e83797d3b2e28
Lay balar <lay.balar05@gmail.com>
2025-08-05 14:44:49 +0530

    first commit

--- backend/userRoutes.js
+++ backend/userRoutes.js
@@ -3,7 +3,7 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
-import User from './user';
+import User from './user.js';
 
 const router = express.Router();
 
